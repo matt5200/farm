c3dd4dcd7b827bbf3dfdb15ab8a140453a914cd2
matthew <leematt5@uw.edu>
2021-12-22 11:06:40 -0800

    improve formatting

--- pages/Api.vue
+++ pages/Api.vue
@@ -4,39 +4,41 @@
         <h1 class="m" >Schedule Your Visit Today!</h1>
       </div>
     <div class="row m-3">
-        <label for="forVisitDate">Date of visit</label>
-        <datepicker class="pb-3" placeholder="Select Date"></datepicker>
-        <div class="form-group pb-3">
-            <label for="inputName">Name</label>
-            <input type="name" class="form-control" id="inputName" aria-describedby="nameHelp" placeholder="Enter name">
-        </div>
-        <div class="form-group pb-3">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-        </div>
-        <div class="form-group pb-3">
-            <div v-if="show">
-                <label for="bringingOthers">Scheduling for a group?</label>
-                <input type="checkbox" class="form-check-input" id="bringingOthers" v-on:click="show = !show">
-            </div>
-            <div v-else>
+      <div class="col">
+          <label for="forVisitDate">Date of visit</label>
+          <datepicker class="pb-3" placeholder="Select Date"></datepicker>
+          <div class="form-group pb-3">
+              <label for="inputName">Name</label>
+              <input type="name" class="form-control" id="inputName" aria-describedby="nameHelp" placeholder="Enter name" required>
+          </div>
+          <div class="form-group pb-3">
+              <label for="exampleInputEmail1">Email address</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+          </div>
+          <div class="form-group pb-3">
+              <div v-if="show">
                   <label for="bringingOthers">Scheduling for a group?</label>
-                <input type="checkbox" class="form-check-input" id="bringingOthers" v-on:click="show = !show">
-                <input type="count" class="form-control" id="inputCount" aria-describedby="nameHelo" placeholder="Party total">
-            </div>
+                  <input type="checkbox" class="form-check-input" id="bringingOthers" v-on:click="show = !show">
+              </div>
+              <div v-else>
+                    <label for="bringingOthers">Scheduling for a group?</label>
+                  <input type="checkbox" class="form-check-input" id="bringingOthers" v-on:click="show = !show">
+                  <input type="count" class="form-control" id="inputCount" aria-describedby="nameHelo" placeholder="Party total">
+              </div>
+          </div>
+          <div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
         </div>
-        <div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
+        <div class="col">
+          <div class="row">
+            <h2> Weather Prediction </h2>
+          </div> 
+          <div class="row">
+          </div> 
+        </div>  
     </div>
   </div>
-
-<!-- <div class=" col m-3">
-      <p>Value: <b>'{{ value }}'</b></p>
-      <p class="mb-0">Context:</p>
-      <pre class="small">{{ context }}</pre>
-    </div> -->
-
 </template>
 
 
